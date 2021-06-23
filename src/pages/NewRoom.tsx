6ba375788e6fe1fb6e49ba8b305c0cb4d3435723
Gustavo <gustavosports7@gmail.com>
2021-06-23 11:35:08 -0300

    :sparkles: Cria autenticação com o google

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -5,9 +5,12 @@ import logoImg from '../assets/images/logo.svg';
 
 import '../styles/auth.scss';
 
-import { Button } from '../components/Button'
+import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth'
 
 export function NewRoom() {
+  const { user } = useAuth();
+  
   return(
     <div id='page-auth'>
         <aside>
@@ -18,8 +21,8 @@ export function NewRoom() {
         <main>
           <div className='main-content'>
             <img src={logoImg} alt='Letmeask' />
+            <h1>{user?.name}</h1>
             <h2>Criar uma nova sala</h2>
-            <div className='separator'>ou entre em uma sala</div>
             <form>
             <input 
               type='text' 
